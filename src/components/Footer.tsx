fa731015f2264360f46e47cd3e3b8bfb817ec2a9
Mihaash <mihaashdharan68@gmail.com>
2026-07-16 18:33:33 +0530

    remove

--- src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="border-t border-border py-6 px-6 text-center text-xs text-text-dim">
-      &copy; 2025 Mihaash Dharan M &nbsp;&middot;&nbsp; Built with passion for security
-    </footer>
-  )
-}
